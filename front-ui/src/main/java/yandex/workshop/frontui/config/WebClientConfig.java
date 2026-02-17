819061dbcac416248b174d65a624566a12f2163e
Nastia Danchenko <nastiadanchenko@gmail.com>
2026-02-17 19:31:21 +0500

    feat: remove spring cloud in front-ui

--- front-ui/src/main/java/yandex/workshop/frontui/config/WebClientConfig.java
+++ front-ui/src/main/java/yandex/workshop/frontui/config/WebClientConfig.java
@@ -1,16 +1,13 @@
 package yandex.workshop.frontui.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
@@ -23,19 +20,17 @@ public class WebClientConfig {
     private final OAuth2AuthorizedClientService authorizedClientService;
 
     @Bean
-    @LoadBalanced
     public WebClient.Builder loadBalancedWebClientBuilder() {
         return WebClient.builder();
     }
 
 
     @Bean
-    public WebClient gatewayWebClient(@LoadBalanced WebClient.Builder builder) {
+    public WebClient gatewayWebClient( WebClient.Builder builder) {
         return builder
             .filter(addAccessTokenHeader())
             .build();
     }
-
 
     private ExchangeFilterFunction addAccessTokenHeader() {
         return (request, next) -> {

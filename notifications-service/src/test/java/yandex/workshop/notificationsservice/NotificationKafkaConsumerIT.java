fc2ccc5745838f13e9667a0b29912157185ffe8e
Nastia Danchenko <nastiadanchenko@gmail.com>
2026-02-23 00:16:58 +0500

    test: create NotificationKafkaConsumerIT, notification-service

---
+++ notifications-service/src/test/java/yandex/workshop/notificationsservice/NotificationKafkaConsumerIT.java
@@ -0,0 +1,57 @@
+package yandex.workshop.notificationsservice;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.org.awaitility.Awaitility;
+import yandex.workshop.notifications_service.api.accounts.model.NotificationRequest;
+import yandex.workshop.notificationsservice.service.NotificationConsumer;
+
+
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("test")
+@Import(TestcontainersConfiguration.class)
+@DirtiesContext
+public class NotificationKafkaConsumerIT {
+
+    @Value("${topic.notification}")
+    public String topicName;
+
+    @Autowired
+    private KafkaTemplate<String, NotificationRequest> kafkaTemplate;
+
+    @SpyBean
+    private NotificationConsumer notificationConsumer;
+
+    @Test
+    void shouldConsumeNotification() throws Exception {
+
+        NotificationRequest request = new NotificationRequest();
+
+        request.setServiceName("accounts-service");
+        request.setUserId("user-1");
+        request.setMessage("Account created");
+        request.setTimestamp(System.currentTimeMillis());
+
+        kafkaTemplate.send(topicName, request);
+
+        Awaitility.await()
+            .atMost(Duration.ofSeconds(10))
+            .untilAsserted(() ->
+                Mockito.verify(notificationConsumer)
+                    .consume(Mockito.any(NotificationRequest.class))
+            );
+
+    }
+
+}

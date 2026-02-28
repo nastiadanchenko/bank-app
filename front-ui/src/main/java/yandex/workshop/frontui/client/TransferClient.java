0246b53565728b9d8b891494a3f0788230883c4f
Nastia Danchenko <nastiadanchenko@gmail.com>
2026-02-28 22:40:10 +0500

    feat: implementation project api-contract into front-ui

--- front-ui/src/main/java/yandex/workshop/frontui/client/TransferClient.java
+++ front-ui/src/main/java/yandex/workshop/frontui/client/TransferClient.java
@@ -4,8 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import yandex.workshop.frontui.api.accounts.model.OperationResponse;
-import yandex.workshop.frontui.api.accounts.model.TransferRequest;
+import yandex.workshop.api.model.OperationResponse;
+import yandex.workshop.api.model.TransferRequest;
 
 @Component
 @RequiredArgsConstructor

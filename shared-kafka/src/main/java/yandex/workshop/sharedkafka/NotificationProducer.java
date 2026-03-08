589c019f86387d548ea89d1e5387e07e8996f55d
Nastia Danchenko <nastiadanchenko@gmail.com>
2026-03-08 18:47:57 +0500

    refactor: add logging to NotificationProducer

--- shared-kafka/src/main/java/yandex/workshop/sharedkafka/NotificationProducer.java
+++ shared-kafka/src/main/java/yandex/workshop/sharedkafka/NotificationProducer.java
@@ -4,6 +4,7 @@ import io.micrometer.core.instrument.MeterRegistry;
 import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
@@ -17,18 +18,39 @@ public class NotificationProducer {
 
     private final MeterRegistry meterRegistry;
 
-    public CompletableFuture<SendResult<String, NotificationRequest>> send(NotificationRequest event, String topicName) {
+    public CompletableFuture<SendResult<String, NotificationRequest>> send(NotificationRequest event,
+                                                                           String topicName) {
+
+        log.debug("Preparing notification event service={} user={} topic={} message={}",
+            event.getServiceName(),
+            event.getLogin(),
+            topicName,
+            event.getMessage());
+
+        log.info("Sending notification to Kafka topic={} user={}",
+            topicName,
+            event.getLogin());
 
         return kafkaTemplate.send(topicName, event)
             .whenComplete((result, ex) -> {
                 if (ex != null) {
-                    log.error("Kafka send failed", ex);
+                    log.error("Kafka send failed topic={} user={} error={}",
+                        topicName,
+                        event.getLogin(),
+                        ex.getMessage(),
+                        ex);
                     meterRegistry.counter(
                         "business.notification.failed",
                         "login", event.getLogin()
                     ).increment();
+
                 } else {
-                    log.info("Kafka send success: {}", result.getRecordMetadata());
+                    RecordMetadata metadata = result.getRecordMetadata();
+                    log.info("Kafka send success topic={} partition={} offset={} user={}",
+                        metadata.topic(),
+                        metadata.partition(),
+                        metadata.offset(),
+                        event.getLogin());
                 }
             });
 
